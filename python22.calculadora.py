4a2bef69b8c993f3c707bbe70965d94ff676b618
cediazta <cdiazt.1987@gmail.com>
2026-01-28 11:53:50 +0100

    while finalizar

--- python22.calculadora.py
+++ python22.calculadora.py
@@ -22,22 +22,36 @@ def menuOpciones():
 print("----- CALCULADORA -----")
 
 # Entrada de datos y Variables
-num1 = int(input("Introduzca el primer numero: "))
-num2 = int(input("Introduzca el segundo numero: "))
-menuOpciones()
-opcion = int(input("Que operacion desea realizar: "))
-resultado = 0
+while True:
+    num1 = int(input("Introduzca el primer numero: "))
+    num2 = int(input("Introduzca el segundo numero: "))
+    menuOpciones()
+    opcion = int(input("Que operacion desea realizar: "))
+    resultado = 0
 
-# Control de flujo de Calculos
-if opcion == 1:
-    resultado = sumar(num1, num2)
-    print(f"El resultado de la suma es {resultado}")
-elif opcion == 2:
-    resultado = restar(num1, num2)
-    print(f"El resultado de la resta es {resultado}")
-elif opcion == 3:
-    resultado = multiplicar(num1, num2)
-    print(f"El resultado de la multiplicacion es {resultado}")
-elif opcion == 4:
-    resultado = dividir(num1, num2)
-    print(f"El resultado de la division es {resultado}")
+    # Control de flujo de Calculos
+    if opcion == 1:
+        resultado = sumar(num1, num2)
+        print(f"El resultado de la suma es {resultado}")
+        respuesta = input("¿Quieres hacer mas operaciones?(Y/N): ").lower()
+        if respuesta == "n":
+            break
+    elif opcion == 2:
+        resultado = restar(num1, num2)
+        print(f"El resultado de la resta es {resultado}")
+        respuesta = input("¿Quieres hacer mas operaciones?(Y/N): ").lower()
+        if respuesta == "n":
+            break
+    elif opcion == 3:
+        resultado = multiplicar(num1, num2)
+        print(f"El resultado de la multiplicacion es {resultado}")
+        respuesta = input("¿Quieres hacer mas operaciones?(Y/N): ").lower()
+        if respuesta == "n":
+            break
+    elif opcion == 4:
+        resultado = dividir(num1, num2)
+        print(f"El resultado de la division es {resultado}")
+        respuesta = input("¿Quieres hacer mas operaciones?(Y/N): ").lower()
+        if respuesta == "n":
+            break
+
