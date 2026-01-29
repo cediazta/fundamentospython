38af0314a27b357a153decc1130319fa1e0d7701
cediazta <cdiazt.1987@gmail.com>
2026-01-29 05:09:37 +0100

    funcion finalizar

--- python22.calculadora.py
+++ python22.calculadora.py
@@ -18,6 +18,12 @@ def menuOpciones():
     print("3. Multiplicar")
     print("4. Dividir")
 
+def finalizar():
+    respuesta = input("¿Quieres hacer mas operaciones?(Y/N): ").lower()
+    return respuesta
+     
+   
+
 # Muestra por pantalla
 print("----- CALCULADORA -----")
 
@@ -33,25 +39,25 @@ while True:
     if opcion == 1:
         resultado = sumar(num1, num2)
         print(f"El resultado de la suma es {resultado}")
-        respuesta = input("¿Quieres hacer mas operaciones?(Y/N): ").lower()
+        respuesta = finalizar()
         if respuesta == "n":
             break
     elif opcion == 2:
         resultado = restar(num1, num2)
         print(f"El resultado de la resta es {resultado}")
-        respuesta = input("¿Quieres hacer mas operaciones?(Y/N): ").lower()
+        respuesta = finalizar()
         if respuesta == "n":
             break
     elif opcion == 3:
         resultado = multiplicar(num1, num2)
         print(f"El resultado de la multiplicacion es {resultado}")
-        respuesta = input("¿Quieres hacer mas operaciones?(Y/N): ").lower()
+        respuesta = finalizar()
         if respuesta == "n":
             break
     elif opcion == 4:
         resultado = dividir(num1, num2)
         print(f"El resultado de la division es {resultado}")
-        respuesta = input("¿Quieres hacer mas operaciones?(Y/N): ").lower()
+        respuesta = finalizar()
         if respuesta == "n":
             break
 
