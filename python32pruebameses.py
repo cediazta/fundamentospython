550b1a4c10a602437f3a826d57a262ec2e865c02
cediazta <cdiazt.1987@gmail.com>
2026-02-02 19:45:05 +0100

    actualizado

--- python32pruebameses.py
+++ python32pruebameses.py
@@ -1,14 +1,19 @@
 from class32mes import Mes
 
 print("Trabajando con clases")
-enero = Mes()
-enero.nombre = "Enero"
-enero.tempMin = -2
-enero.tempMax = 9
-print(f"Media Enero {enero.getTempMedia()} grados.")
+listaMeses = []
+for i in range(1, 4):
+    mes = Mes()
+    mes.nombre = input(f"Introduzca el mes {i}: ")
+    mes.tempMax = int(input("Temperatura Maxima: "))
+    mes.tempMin = int(input("Temperatura Minima: "))
+    listaMeses.append(mes)
 
-febrero = Mes()
-febrero.nombre = "Febrero"
-febrero.tempMin = 5
-febrero.tempMax = 15
-print(f"Media Febrero {febrero.getTempMedia()} grados.")
+
+for dato in listaMeses:
+    print(f"\n{dato.nombre}:")
+    print(f"Maxima {str(dato.tempMax)}.")
+    print(f"Minima {str(dato.tempMin)}.")
+    print(f"Media {dato.getTempMedia()}.")
+
+print("Fin de programa")
