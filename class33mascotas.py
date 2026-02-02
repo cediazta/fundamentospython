0263fde34d28059a35420a24f8ccfd4c31d370c2
cediazta <cdiazt.1987@gmail.com>
2026-02-02 20:08:45 +0100

    actualizado

--- class33mascotas.py
+++ class33mascotas.py
@@ -1,7 +1,7 @@
 class Mascotas:
     def __init__(self):
         self.nombre = ""
-        self.raza = ""
+        self.tipo = ""
         self.anyoNacimiento = 0
         self.anyoAdopcion = 0
     
--- python33mascotas.py
+++ python33mascotas.py
@@ -6,14 +6,14 @@ animales = []
 for i in range (1,2):
     animal = Mascotas()
     animal.nombre = input("Introduce el nombre de la mascota: ")
-    animal.raza = input("Introduce la raza del animal: ")
+    animal.tipo = input("Introduce el tipo de animal: ")
     animal.anyoNacimiento = int(input("Introduce el año de nacimiento: "))
     animal.anyoAdopcion = int(input("Introduzca el año de adopcion: "))
     animales.append(animal)
 
 for dato in animales:
     print(f"\nNombre: {animal.nombre}.")
-    print(f"Raza: {animal.raza}")
+    print(f"Tipo: {animal.tipo}")
     print(f"Año de nacimiento: {animal.anyoNacimiento}")
     print(f"Anyo de adopcion: {animal.anyoAdopcion}")
     print(f"Años adoptada: {dato.getAnyosAdoptada()}")
