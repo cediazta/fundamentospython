2673ce33bac1427768be300966ef95faf196a757
cediazta <cdiazt.1987@gmail.com>
2026-02-03 12:42:27 +0100

    nuevos

--- libreria23matematicas.py
+++ libreria23matematicas.py
@@ -1,1 +1,22 @@
+# Funciones
+def sumar(num1, num2):
+    return num1 + num2
 
+def restar(num1, num2):
+    return num1 - num2
+
+def multiplicar(num1, num2):
+    return num1 * num2
+
+def dividir(num1, num2):
+    return num1 / num2
+
+def menuOpciones():
+    print("1. Sumar")
+    print("2. Restar")
+    print("3. Multiplicar")
+    print("4. Dividir")
+
+def finalizar():
+    respuesta = input("¿Quieres hacer mas operaciones?(Y/N): ").lower()
+    return respuesta
---
+++ python23matematicas.py
@@ -0,0 +1,38 @@
+from libreria23matematicas import *
+
+# Muestra por pantalla
+print("----- CALCULADORA -----")
+
+# Entrada de datos y Variables
+while True:
+    num1 = int(input("Introduzca el primer numero: "))
+    num2 = int(input("Introduzca el segundo numero: "))
+    menuOpciones()
+    opcion = int(input("Que operacion desea realizar: "))
+    resultado = 0
+
+    # Control de flujo de Calculos
+    if opcion == 1:
+        resultado = sumar(num1, num2)
+        print(f"El resultado de la suma es {resultado}")
+        respuesta = finalizar()
+        if respuesta == "n":
+            break
+    elif opcion == 2:
+        resultado = restar(num1, num2)
+        print(f"El resultado de la resta es {resultado}")
+        respuesta = finalizar()
+        if respuesta == "n":
+            break
+    elif opcion == 3:
+        resultado = multiplicar(num1, num2)
+        print(f"El resultado de la multiplicacion es {resultado}")
+        respuesta = finalizar()
+        if respuesta == "n":
+            break
+    elif opcion == 4:
+        resultado = dividir(num1, num2)
+        print(f"El resultado de la division es {resultado}")
+        respuesta = finalizar()
+        if respuesta == "n":
+            break
