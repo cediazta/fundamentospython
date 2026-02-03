2ae0257a4357613e912f75d522c24c4f99aa548e
cediazta <cdiazt.1987@gmail.com>
2026-02-03 12:43:59 +0100

    actualizado

--- python30mainpersonas.py
+++ python30mainpersonas.py
@@ -2,10 +2,10 @@ from class30persona import Persona
 print("Manejando clases persona")
 # Creamos una persona
 persona1 = Persona()
-persona1.nombre = "Cesar"
-persona1.apellidos = "Diaz Tardon"
-persona1.anyoNacimiento = 1987
-persona1.email = "cdiazt.1987@gmail.com"
+persona1.nombre = "Pepito"
+persona1.apellidos = "Gomez Pere<"
+persona1.anyoNacimiento = 1967
+persona1.email = "pepito19@gmail.com"
 persona1.pais = "España"
 print(persona1.getNombreCompleto())
 print(f"Edad {persona1.nombre}: {persona1.getEdad()}")
