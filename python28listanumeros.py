295f1c745db6cbaf8a7204ce74253d948752d086
cediazta <cdiazt.1987@gmail.com>
2026-02-03 12:22:09 +0100

    actualizado

--- python28listanumeros.py
+++ python28listanumeros.py
@@ -1,1 +1,36 @@
-# HACERLO EN CASA
+## LISTA NUMEROS
+
+# Variables
+numero = 0
+suma = 0
+sumaPares = 0
+sumaImpares = 0
+listaNumeros = []
+listaPares = []
+listaImpares = []
+
+# Bucle
+while True:
+    num = int(input("Introduzca numeros, (para terminar escriba -1): "))
+    if num != -1:
+        listaNumeros.append(num)
+        suma += num
+        if num % 2 == 0:
+            listaPares.append(num)
+            sumaPares += num
+        else:
+            listaImpares.append(num)  
+            sumaImpares += num
+    else:
+        break
+    
+
+# Mostrar por Pantalla
+print(f"La suma total de los numeros es {suma}.")
+print(f"Numeros introducidos:  {len(listaNumeros)}")
+print(f"Numeros pares: {len(listaPares)}")
+print(f"Numeros impares: {len(listaImpares)}")
+print(f"Suma pares: {sumaPares}")
+print(f"Suma impares: {sumaImpares}")
+
+
--- python29narcisista.py
+++ python29narcisista.py
@@ -1,2 +1,24 @@
-# HACERLO EN CASA
+## NUMERO NARCISISTA
+
+# Variables
+numero = input("Introduce un numero: ")
+listanumeros = []
+suma = 0
+numero = numero
+
+# Bucle
+for i in range(len(numero)):
+    num = int(numero[i])
+    potencia = num ** 3
+    listanumeros.append(potencia)
+    suma += potencia
+    
+# Control de Flujo    
+if suma == int(numero):
+    print("Narcisista!")
+else:
+    print("No es narcisista.")
+
+
+
 
